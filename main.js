2d4a4c5e6eb7433ba2702cf55961587d1fde9c3a
unknown <codeleap@outlook.com>
2019-04-11 21:48:21 +0200

    calculateOutput method completed. PrintResults method added to CalculatePrint class.

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 function CalculatePrint(years){
 	this.years = years;
 
-	this.calculateMonthlyTargets = function(){
-
+	this.calculateOutput = function(inputValue){
+		let outputString = "";
+		outputString += `<h3>To save $${inputValue} you would need to save:</h3>`;
+		for(i=0; i<years.length; i++){
+			mths = years[i] * 12;
+			mthTarget = (inputValue/mths).toFixed(2);
+			outputString += `<p>$${mthTarget} per month for ${mths} months.</p>`;
+		}
+        this.printResults(outputString);
 	}
 
-	this.printResults = function(){
-        
+	this.printErrorMessage = function(message){
+		displayResultsDiv.innerHTML = "<h1>" + message + "</h1>";
+	}
+
+	this.printResults = function(outputString){
+		displayResultsDiv.innerHTML = "<h1>" + outputString + "</h1>";
 	}
 	
 } 
@@ -15,8 +26,9 @@ function Validate(inputValue){
     this.inputValue = inputValue;
 
 	this.validateInput = function(){
-        if(this.inputValue) return true; 
-        else return false; 
+        if(isNaN(this.inputValue)) return "Your input is not a valid number"; 
+        if(this.inputValue <= 0) return "Your input must be greater than 0";
+        return "valid";
 	}
 }
 
@@ -27,10 +39,13 @@ var displayResultsDiv = document.getElementById("displayResults");
 //array of number of years added to function constructor
 var calcPrint = new CalculatePrint([0.5,1,2,3,4,5,10]);
 
-
 button.onclick = function(){
 	var validate = new Validate(input.value);
-	console.log(validate.validateSetInput());
+	if(validate.validateInput() == "valid"){
+        calcPrint.calculateOutput(input.value);
+	}else{
+        calcPrint.printErrorMessage(validate.validateInput());
+	}
 }
 
 
