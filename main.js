c7449447fb4dc46305c3f7945b860c009c6dc09f
unknown <codeleap@outlook.com>
2019-04-11 20:15:06 +0200

    Divided main class into 2 classes - CalculatePrint and a Validate class.

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 function CalculatePrint(years){
 	this.years = years;
 
-    this.validateSetInput = function(inputValue){
-
-    }
-
 	this.calculateMonthlyTargets = function(){
 
 	}
@@ -15,13 +11,26 @@ function CalculatePrint(years){
 	
 } 
 
+function Validate(inputValue){
+    this.inputValue = inputValue;
+
+	this.validateInput = function(){
+        if(this.inputValue) return true; 
+        else return false; 
+	}
+}
+
 var input = document.getElementById("savingsTargetInput");
 var button = document.getElementById("calcMonthlyTargetBtn");
+var displayResultsDiv = document.getElementById("displayResults");
 
+//array of number of years added to function constructor
 var calcPrint = new CalculatePrint([0.5,1,2,3,4,5,10]);
 
+
 button.onclick = function(){
-	myClass.validateSetInput(input.value);
+	var validate = new Validate(input.value);
+	console.log(validate.validateSetInput());
 }
 
 
